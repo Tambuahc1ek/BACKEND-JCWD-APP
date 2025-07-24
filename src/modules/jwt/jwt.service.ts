c9fe500e65bb90561567d7672bfd7a814428bf8d
Tambuahc1ek <133321330+Tambuahc1ek@users.noreply.github.com>
2025-07-24 15:53:12 +0700

    feat: add login (#1)

---
+++ src/modules/jwt/jwt.service.ts
@@ -0,0 +1,6 @@
+import { sign, SignOptions } from "jsonwebtoken";
+export class JwtService {
+    generateToken = (payload: any, secretKey: string, options: SignOptions) => {
+        return sign(payload, secretKey, options);
+    }
+}
